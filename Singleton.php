6f12896732e6d324f0b163788690c9cfa2f86298
Jiehui Chen <599194429@qq.com>
2017-08-29 21:45:08 +0800

    no message

---
+++ Singleton.php
@@ -0,0 +1,40 @@
+<?php 
+
+/**
+ * 抽象工厂方法
+ **/
+
+ class Preferences 
+ {
+    private $props = array();
+    private static $instance;
+
+    private function __construct(){
+    }
+    
+    public static function getInstance(){
+
+      if (empty(self::$instance)){
+          self::$instance = new Preferences();
+      }
+      return self::$instance;
+    }
+
+    public function setProperty($key,$val){
+
+      $this->props[$key] = $val;
+
+    }
+
+    public function getProperty($key){
+
+      return $this->props[$key];
+
+    }
+ 
+}
+//调用单例
+  $pref = Preferences::getInstance();
+  $pref->setProperty('name','cjhlfy');
+  $pref2 = Preferences::getInstance();
+  echo $pref2->getProperty('name'); //name的属性值不会丢失,因为实例引用$instance是静态属性
